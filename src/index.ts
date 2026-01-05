50e874677992afbcd43ab2e6956902ee03573697
Pravin Bhattarai <pravinbhattarai.cse2022@ritroorkee.com>
2026-01-05 17:05:07 +0530

    jpt

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const prisma = new PrismaClient();
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN || "localhost:3000",
+  origin: process.env.FRONTEND_ORIGIN || "https://six-loan.vercel.app/",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
